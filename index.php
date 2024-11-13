55bce23fe5df34b86e6222330c2308ce7dc3af5d
Anthony MEUNIER <anthony.meunier@lyon.ort.asso.fr>
2024-11-13 22:14:31 +0100

    motus game

---
+++ index.php
@@ -0,0 +1,68 @@
+<?php
+
+use BO\GameMotus;
+require __DIR__ . "/vendor/autoload.php";
+
+require __DIR__ ."/src/model/BO/gameMotus.php";
+
+require __DIR__ ."/src/model/words.json";
+
+
+$listWord = json_decode(file_get_contents(__DIR__ ."/src/model/words.json"), true);
+$word = $listWord[array_rand($listWord)];
+var_dump($word);
+
+$motus = new  GameMotus($word);
+
+$try = 1;
+
+while (true) {
+    $motus->clear();
+    if($try >10)
+    {
+        echo"You lose this Game !!!\r\n\r\n";
+        echo "The word is ".$motus->getWord()."\r\n\r\n";
+
+        exit;
+    }
+
+
+
+    echo "MOTUS GAME \r\n ". "try :".$try."/10\r\n\r\n\r\n";
+    
+    $motus->displayWord();
+
+
+	$line = strtoupper(str_replace(" ", "", trim(fgets( STDIN))));
+
+    if (strlen($line) !== count($motus->getHidingWord()))
+    {
+        $try ++;
+        continue;
+    }
+
+    
+    $isFinish = $motus->gameFinish($line);
+    
+    if ($isFinish) {
+        echo "You win the game !!!!";
+        exit;
+    }
+
+    if ($line == "") {
+        continue;
+    }
+
+    $motus->checkWordCorrespondence($line);
+
+
+
+
+
+
+ $try ++;
+}
+
+
+
+
---
+++ src/model/BO/gameMotus.php
@@ -0,0 +1,116 @@
+<?php
+
+namespace BO;
+
+class GameMotus {
+    private string $word;
+    private array $wordSplit;
+    private array $hidingWord;
+    public array $listeTentative = [];
+
+    public function getWord():string
+    {
+        return $this->word;
+    } 
+    public function setWord(string $word): static
+    {
+        $this->word = $word;
+        return $this;
+    }
+
+    public function getWordSplit():array
+    {
+        return $this->wordSplit;
+    }
+
+
+    public function setWordSplit(array $wordSplit): static
+    {
+        $this->wordSplit = $wordSplit;
+        return $this;
+    }
+
+    public function getHidingWord():array
+    {
+        return $this->hidingWord;
+    }
+    public function setHidingWord(array $hidingWord): static
+    {
+        $this->hidingWord = $hidingWord;
+        return $this;
+    }
+
+    public function __construct(string $data) 
+    {
+        $data = strtoupper($data);
+        $this->word = $data;
+        $this->wordSplit = str_split($data);
+        $this->hidingWord = $this->hiddingWord($data);
+    }
+
+
+    public function hiddingWord(string $data): array
+    {
+        $result = array();
+        array_push($result, mb_substr($data,0,1));
+        for($i=1; $i<mb_strlen($data); $i++)
+        {
+            array_push($result, "0");
+        }
+
+
+        return $result;
+    }
+
+
+    public function checkWordCorrespondence(string $inputWord): void
+    {
+        $result = "";
+        $arrayInput = str_split($inputWord);
+        for($i= 1; $i<count($arrayInput); $i++)
+        {
+            if($this->wordSplit[$i] == $arrayInput[$i])
+            {
+                $this->hidingWord[$i] = $arrayInput[$i];
+            }
+        }
+        //array_push($this->listeTentative, $this->hidingWord);
+    }
+
+
+    public function displayWord():void{
+        
+        for($i= 0; $i<count($this->hidingWord); $i++)
+        {
+            switch($this->hidingWord[$i])
+            {
+                case "0":
+                    echo "_ ";
+                    break;
+                case "1":
+                    echo "° ";
+                    break;
+                default: 
+                    echo ($this->hidingWord[$i] . " ");
+                    break;
+            }
+
+
+
+            //echo ($this->hidingWord[$i] . " ");
+        }
+        echo "\r\n\r\n";
+    }
+
+
+    public function clear(): void
+    {
+        echo chr(27).chr(91).'H'.chr(27).chr(91).'J';
+    }
+
+
+    public function gameFinish (string $inputWord): bool
+    {
+        return $this->word === $inputWord;
+    }
+}
